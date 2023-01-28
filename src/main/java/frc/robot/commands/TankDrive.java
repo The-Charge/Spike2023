29d314dc41a4482f3f6a11fb368a34dc493f4bf1
Andrew Menzl <ammenzl7@users.noreply.github.com>
2023-01-28 09:04:28 -0500

    Drivetrain Fix (#5)

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -1,20 +1,17 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-public class Tankdrive extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
+public class TankDrive extends CommandBase {
   private final Drivetrain m_drivetrain;
+
   /**
    *
    * @param subsystem The subsystem used by this command.
    */
-  public Tankdrive(Drivetrain subsystem) {
+  public TankDrive(Drivetrain subsystem) {
     m_drivetrain = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
@@ -22,7 +19,9 @@ public class Tankdrive extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {m_drivetrain.initializeMotors();}
+  public void initialize() {
+    m_drivetrain.initializeMotors();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -31,12 +30,13 @@ public class Tankdrive extends CommandBase {
     rightSpeed = -RobotContainer.getInstance().getrightJoystick().getY();
     leftSpeed = -RobotContainer.getInstance().getleftJoystick().getY();
 
-     m_drivetrain.run(leftSpeed, rightSpeed);
-}
+    m_drivetrain.run(leftSpeed, rightSpeed);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,122 +1,59 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.DemandType;
-import com.ctre.phoenix.motorcontrol.FollowerType;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
-import com.ctre.phoenix.motorcontrol.StatusFrame;
-import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
-import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.kauailabs.navx.frc.AHRS;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
-import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
-import edu.wpi.first.wpilibj.SerialPort.Port; //might change to I2C
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  
+  private WPI_TalonFX leftFrontMotor;
+  private WPI_TalonFX leftBackMotor;
+  private WPI_TalonFX rightFrontMotor;
+  private WPI_TalonFX rightBackMotor;
 
-private WPI_TalonFX leftFrontMotor;
-private WPI_TalonFX leftBackMotor;
-private WPI_TalonFX rightFrontMotor;
-private WPI_TalonFX rightBackMotor;
+  private MotorControllerGroup m_leftMotors;
+  private MotorControllerGroup m_rightMotors;
 
-
-private MotorControllerGroup m_leftMotors; 
-
-private MotorControllerGroup m_rightMotors; 
-
-// private final AHRS m_gyro = new AHRS(Port.kMXP);
-// private double gyroOffset = 0;
-
-public Drivetrain() {
-  leftFrontMotor = new WPI_TalonFX(1);
-  leftBackMotor = new WPI_TalonFX(2);
-  rightFrontMotor = new WPI_TalonFX(3);
-  rightBackMotor = new WPI_TalonFX(4);
-  m_leftMotors = new MotorControllerGroup(leftFrontMotor, leftBackMotor);
-	m_rightMotors = new MotorControllerGroup(rightFrontMotor, rightBackMotor);
-}
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public CommandBase exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
+  public Drivetrain() {
+    leftFrontMotor = new WPI_TalonFX(1);
+    leftBackMotor = new WPI_TalonFX(2);
+    rightFrontMotor = new WPI_TalonFX(3);
+    rightBackMotor = new WPI_TalonFX(4);
+    m_leftMotors = new MotorControllerGroup(leftFrontMotor, leftBackMotor);
+    m_rightMotors = new MotorControllerGroup(rightFrontMotor, rightBackMotor);
   }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
+  public void initializeMotors() {
+    leftFrontMotor.configFactoryDefault();
+    rightFrontMotor.configFactoryDefault();
+
+    rightFrontMotor.setInverted(true);
+    rightBackMotor.setInverted(true);
+
+    leftBackMotor.follow(leftFrontMotor);
+    rightBackMotor.follow(rightFrontMotor);
+
+    leftFrontMotor.configOpenloopRamp(0.5);
+    leftBackMotor.configOpenloopRamp(0.5);
+    rightFrontMotor.configOpenloopRamp(0.5);
+    rightBackMotor.configOpenloopRamp(0.5);
+
+    leftFrontMotor.configNeutralDeadband(0.08);
+    rightFrontMotor.configNeutralDeadband(0.08);
   }
 
+  public void run(double l, double r) {
+    leftFrontMotor.set(l);
+    rightFrontMotor.set(r);
+  }
+
+  // This method will be called once per scheduler run
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-
   }
 
+  // This method will be called once per scheduler run during simulation
   @Override
   public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
   }
-  public void run(double l, double r) {
-		leftFrontMotor.set(l);
-		rightFrontMotor.set(r);
-	}
-  public void initializeMotors() {
 
-		leftFrontMotor.configFactoryDefault();
-		rightFrontMotor.configFactoryDefault();
-
-		rightBackMotor.setInverted(true);
-		rightFrontMotor.setInverted(true);
-
-		rightBackMotor.follow(rightFrontMotor);
-		leftBackMotor.follow(leftFrontMotor);
-
-		rightBackMotor.configOpenloopRamp(0.5);
-		rightFrontMotor.configOpenloopRamp(0.5);
-		leftBackMotor.configOpenloopRamp(0.5);
-		leftFrontMotor.configOpenloopRamp(0.5);
-
-		leftFrontMotor.configNeutralDeadband(0.08);
-		rightFrontMotor.configNeutralDeadband(0.08);
-
-
-    }
 }
